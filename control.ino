686e06fab5184f68a72ee896c7839ff2858e5853
loveket <906459896@qq.com>
2024-11-06 15:53:30 +0800

    second

--- control.ino
+++ control.ino
@@ -1,12 +1,13 @@
 #include <ESP8266WiFi.h>        // 本程序使用 ESP8266WiFi库
-#include <ESP8266WiFiMulti.h>   //  ESP8266WiFiMulti库
 #include <ESP8266WebServer.h>   //  ESP8266WebServer库
 #include <DHT.h>
 //io定义
 #define FanIO  D6
 #define TWIO  D2
 #define DHTTYPE DHT11
-ESP8266WiFiMulti wifiMulti;     // 建立ESP8266WiFiMulti对象,对象名称是'wifiMulti'
+
+const char* ssid     = "zcc333";                // 需要连接到的WiFi名
+const char* password = "15144099398b";             // 连接的WiFi密码
 ESP8266WebServer esp8266_server(3333);// 建立网络服务器对象，该对象用于响应HTTP请求。监听端口（80）
 IPAddress local_IP(192, 168, 0, 70); // 设置ESP8266-NodeMCU联网后的IP
 IPAddress gateway(192, 168, 0, 1);    // 设置网关IP（通常网关IP是WiFI路由IP）
@@ -16,51 +17,53 @@ DHT dht(TWIO, DHTTYPE);
 bool FanStatus=false;
 bool IsHanderOn=false;
 float tempGet=0.0;
+//定时轮询
 unsigned long previousMillis = 0;
-unsigned long interval = 15000;
+unsigned long interval = 10000;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  //初始化http服务
+  initWifi();
   initWebServer();
+  initDht();
   pinMode(FanIO,OUTPUT);
-  dht.begin();
 }
-void initWebServer(){
+void initWifi(){
   // 设置开发板网络环境
   if (!WiFi.config(local_IP, gateway, subnet)) {
     Serial.println("Failed to ESP8266 IP"); 
     return;
   }
-  wifiMulti.addAP("zcc333", "15144099398b"); // 将需要连接的一系列WiFi ID和密码输入这里
-  Serial.println("Connecting ..."); 
-  int i = 0;
-  bool isExe=false;                                 
-  while (wifiMulti.run() != WL_CONNECTED) {  // 此处的wifiMulti.run()是重点。通过wifiMulti.run()，NodeMCU将会在当前
-    delay(1000);                             // 环境中搜索addAP函数所存储的WiFi。如果搜到多个存储的WiFi那么NodeMCU
-    Serial.print(i++); Serial.print(' ');    // 将会连接信号最强的那一个WiFi信号。
-    if(i>=5){
-      isExe=true;
-      break;
-     }
-  }                                          // 一旦连接WiFI成功，wifiMulti.run()将会返回“WL_CONNECTED”。这也是
-                                             // 此处while循环判断是否跳出循环的条件。
-  if(!isExe){
-    // WiFi连接成功后将通过串口监视器输出连接成功信息 
+  WiFi.mode(WIFI_STA);                       // 设置Wifi工作模式为STA,默认为AP+STA模式
+  WiFi.begin(ssid, password);
+  int i = 0;                                    // 检查WiFi是否连接成功
+  while (WiFi.status() != WL_CONNECTED)         // WiFi.status()函数的返回值是由NodeMCU的WiFi连接状态所决定的。 
+  {                                             // 如果WiFi连接成功则返回值为WL_CONNECTED
+    delay(1000);                                // 此处通过While循环让NodeMCU每隔一秒钟检查一次WiFi.status()函数返回值
+    Serial.print("waiting...");                          
+    Serial.print(i++);
+    Serial.println('\n');
+  }
+  // WiFi连接成功后将通过串口监视器输出连接成功信息 
   Serial.println('\n');
   Serial.print("Connected to ");
   Serial.println(WiFi.SSID());              // 通过串口监视器输出连接的WiFi名称
   Serial.print("IP address:\t");
   Serial.println(WiFi.localIP());           // 通过串口监视器输出ESP8266-NodeMCU的IP
-   // 设置当网络断开连接的时候自动重连
+  // 设置当网络断开连接的时候自动重连
   WiFi.setAutoReconnect(true);
   WiFi.persistent(true);
-  esp8266_server.begin();                           // 启动网站服务
-  esp8266_server.on("/off", handleFanOff);  // 设置处理LED控制请求的函数'handleLED'
-  esp8266_server.on("/on",  handleFanOn);  // 设置处理LED控制请求的函数'handleLED'
+}
+void initWebServer(){
+  //启动http服务
+  esp8266_server.begin();                           
+  esp8266_server.on("/off", handleFanOff); 
+  esp8266_server.on("/on",  handleFanOn); 
+  esp8266_server.onNotFound(handleNotFound);
   Serial.println("HTTP esp8266_server started");//  告知用户ESP8266网络服务功能已经启动
-   }
-  
+}
+void initDht(){
+  dht.begin();
 }
 void handleFanOff(){
   if(!FanStatus){
@@ -84,8 +87,11 @@ void handleFanOn(){
    IsHanderOn=true;
    esp8266_server.send(200, "text/plain", "open fan");
 }
+void handleNotFound(){                                              // 当浏览器请求的网络资源无法在服务器找到时，
+  esp8266_server.send(404, "text/plain", "404: Not found");         // NodeMCU将调用此函数。
+}
 void GetTempAndWet(){
-  //每隔15s读取
+  //每隔10s读取
   unsigned long currentMillis = millis();
   if(currentMillis - previousMillis >=interval){
     //wetGet = dht.readHumidity();//读湿度
@@ -108,7 +114,6 @@ void GetTempAndWet(){
  }
 void loop() {
   //Serial.println(digitalRead(FanIO));
-  //delay(10000);
   esp8266_server.handleClient();
   //获取温湿度数据
   GetTempAndWet();
